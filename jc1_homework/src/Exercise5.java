80f4fc790885f9903c2ec4f4462acbaf05b8ade4
yanadanovskaya <91852932+yanadanovskaya@users.noreply.github.com>
2023-03-27 18:17:10 +0300

    Add files via upload

---
+++ jc1_homework/src/Exercise5.java
@@ -0,0 +1,10 @@
+public class Exercise5 {
+    public static void main(String[] args) {
+        System.out.println(sumOfTwoNumbers(3, 2));
+    }
+    static int sumOfTwoNumbers(int a, int b){
+
+        return a + b;
+    }
+
+}
