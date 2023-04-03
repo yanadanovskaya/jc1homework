ff7fc06189ae271da1dfff08704dd7426ffa27ee
yanadanovskaya <91852932+yanadanovskaya@users.noreply.github.com>
2023-04-03 14:53:21 +0300

    Add files via upload

--- jc1_homework/src/part2/task20/Exercise20.java
+++ jc1_homework/src/part2/task20/Exercise20.java
@@ -1,15 +1,15 @@
-package part2.task19;
+package part2.task20;
 
-public class Exercise19 {
+public class Exercise20 {
     public static void main(String[] args) {
-
-      int arr[]=new int[10];
+        int arr[];
+        arr = new int[10];
         for (int i=0;i<arr.length;i++)
             arr[i] = (int) ( Math.random() * 10);
         for (int i: arr)
             System.out.println( i + " " );
-        System.out.println("Последний элемент массива:" + arr[9]);
+        System.out.println(arr[1] + " " + arr[3] + " " + arr[5] + " " + arr[7] + " " + arr[9]);
+    }
     }
 
 
-}
