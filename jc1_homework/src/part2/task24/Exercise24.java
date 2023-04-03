fdb477e7531b988a87c822eadb4bf81e04f2659f
yanadanovskaya <91852932+yanadanovskaya@users.noreply.github.com>
2023-04-03 16:11:26 +0300

    Add files via upload

---
+++ jc1_homework/src/part2/task24/Exercise24.java
@@ -0,0 +1,35 @@
+package part2.task24;
+
+public class Exercise24 {
+    public static void main(String[] args) {
+        int arr[][] = new int[5][5];
+        int i, j;
+
+
+        for (i = 0; i < arr.length / 2 + 1; i++) {
+            for (j = 0; j < arr[i].length; j++) {
+                if ((j < i) || (j >= (arr[i].length - i)))
+                    arr[i][j] = 0;
+                else
+                    arr[i][j] = 1;
+
+            }
+
+        }
+        for (i = arr.length - 1; i >= arr.length / 2 + 1; i--) {
+            for (j = 0; j < arr[i].length; j++) {
+                if ((j < (arr[i].length - 1 - i)) || (j > i))
+                    arr[i][j] = 0;
+                else
+                    arr[i][j] = 1;
+            }
+        }
+        for (i = 0; i < arr.length; i++) {
+            for (j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println(" ");
+        }
+    }
+
+}
