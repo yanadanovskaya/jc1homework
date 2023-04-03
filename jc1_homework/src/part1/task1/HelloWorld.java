394a23f33c8ef20a5659e8a64b73f2f400273c85
yanadanovskaya <91852932+yanadanovskaya@users.noreply.github.com>
2023-04-03 09:45:35 +0300

    Delete jc1_homework directory

--- jc1_homework/src/part1/task1/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package part1.task1;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-
-        System.out.println("Hello World!");
-    }
-}
-
--- jc1_homework/src/part1/task3/Exercise3.java
+++
@@ -1,7 +0,0 @@
-package part1.task3;
-
-public class Exercise3 {
-        public static void main(String[] args) {
-            System.out.println("Hello Belarus!\nI like Java!");
-        }
-    }
