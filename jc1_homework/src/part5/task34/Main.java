3ab102ddf4da86d15506cae8e46669679f1e3443
yanadanovskaya <91852932+yanadanovskaya@users.noreply.github.com>
2023-04-26 19:04:31 +0300

    Add files via upload

---
+++ jc1_homework/src/part5/task34/Main.java
@@ -0,0 +1,15 @@
+package part5.task34;
+
+public class Main {
+    public static void main(String[] args) {
+        Card card1 = new BelagropromBank("BelagropromBank ",5470-1584-5643-9994,2175,20_000.00);
+        card1.withdrawCash(1000);
+
+        Card card2 = new PriorBank("Prior Bank ",5470-8740-5177-9990,6080,456.32);
+        card2.withdrawCash(300);
+
+        Card card3 = new BelarusBank("Belarusbank",8765-9991-5987-4673,7241,7658.50);
+        card3.withdrawCash(5555);
+
+    }
+}
---
+++ jc1_homework/src/part5/task34/PriorBank.java
@@ -0,0 +1,27 @@
+package part5.task34;
+
+public class PriorBank extends Card {
+    private String bankName;
+    private int cardId;
+    private int password;
+    private double balance;
+
+    public PriorBank(String bankName, int cardId, int password, double balance) {
+        this.bankName = bankName;
+        this.cardId = cardId;
+        this.password = password;
+        this.balance = balance;
+
+    }
+
+
+    @Override
+    protected void withdrawCash(int balance) {
+        if (this.balance >= balance) {
+            this.balance -= balance;
+            System.out.println("Your balance after operation is " + this.balance + " RUB");
+
+
+        }
+    }
+}
